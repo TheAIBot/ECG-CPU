aea1cdc981e412526eaa27d7b3d0a70e44761c65
TheAIBot <andreasuranus@hotmail.com>
2016-10-13 20:01:31 +0200

    partially fixed bus

--- UCI_programs/compile.sh
+++ UCI_programs/compile.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-if [[ -z $1 ]]; then
-	java -jar UCIC.jar program.uci
-else
-	if [[ -z $2 ]]; then
-		java -jar UCIC.jar $1 program.asm
-	else
-		java -jar UCIC.jar $1 $2
-	fi
-fi
+java -jar UCIC.jar "$@"
